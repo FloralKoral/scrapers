c8f90440c8386aed736128da386335c836fb6148
FloralKoral <98429097+FloralKoral@users.noreply.github.com>
2022-02-01 17:43:16 -0600

    define imports

---
+++ main.py
@@ -0,0 +1,8 @@
+import pickle
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from string import ascii_lowercase
+import os
+
