e016ea63abe76a5a30c0c5cb8b3b4f6da8aaccfe
FloralKoral <98429097+FloralKoral@users.noreply.github.com>
2022-02-01 19:59:04 -0600

    implemented classes to pass data between definitions with ease

--- main.py
+++ main.py
@@ -6,52 +6,86 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from string import ascii_lowercase
 import os
 import sqlite3
+from sqlite3 import Error
+
 
 # DATA
 url_dafont = 'https://www.dafont.com/alpha.php?lettre={}&page={}&fpp=200'
 keys = [ascii for ascii in ascii_lowercase] + ['%23']
 values = [url_dafont.format(values, '1') for values in keys]
 
-con = sqlite3.connect("data_dafont.db")
-cur = con.cursor()
 
-def getTable():
-    with con:
-        cur.execute("SELECT * FROM url_data")
-        print(cur.fetchall())
+class sql_shit:
 
-def insertVaribleIntoTable(lettre=None, page1_url=None, page_count=None):
-    try:
-        print("Connected to SQLite")
+    def create_connection(self, db_file):
+        """ create a database connection to the SQLite database
+            specified by the db_file
+        :param db_file: database file
+        :return: Connection object or None
+        """
+        self.conn = None
+        self.cur = None
+        try:
+            self.conn = sqlite3.connect(db_file)
+            print("Connected to SQLite")
+        except Error as e:
+            print("Failed to connect to database",e)
 
-        sqlite_insert_with_param = """INSERT INTO url_data
-                          (lettre, page1_url, page_count) 
-                          VALUES (?, ?, ?);"""
+        return self.conn
 
-        data_tuple = (lettre, page1_url, page_count)
-        cur.execute(sqlite_insert_with_param, data_tuple)
-        con.commit()
-        print("Python Variables inserted successfully into SqliteDb_developers table")
+    def getTable(self):
+        try:
+            cur = self.conn.cursor()
+            cur.execute("SELECT * FROM url_data")
+            print(cur.fetchall())
+        except Error as e:
+            print("\nERROR:", e, "\ncheck database name or something fuck you")
 
-    except sqlite3.Error as error:
-        print("Failed to insert Python variable into sqlite table", error)
+    def insertVaribleIntoTable(self, lettre=None, page1_url=None, page_count=None):
+        try:
+            sqlite_insert_with_param = """INSERT INTO url_data
+                              (lettre, page1_url, page_count) 
+                              VALUES (?, ?, ?);"""
 
-def deleteAll():
-    with con:
+            data_tuple = (lettre, page1_url, page_count)
+            cur = self.conn.cursor()
+            cur.execute(sqlite_insert_with_param, data_tuple)
+            self.conn.commit()
+            print("Python Variables inserted successfully into SqliteDb_developers table")
+
+        except Error as e:
+            print("Failed to insert Python variable into sqlite table", e)
+
+    def deleteAll(self):
+        cur = self.conn.cursor()
         cur.execute('DELETE FROM url_data;')
         print(cur.fetchall())
 
-def initial_data_population():
-    for i in range(len(keys)):
-        insertVaribleIntoTable(lettre=keys[i], page1_url=values[i])
-    con.commit()
+    # def initial_data_population(self):
+    #     for i in range(len(keys)):
+    #         insertVaribleIntoTable(lettre=keys[i], page1_url=values[i])
+    #     con.commit()
 
-def cursor_iteration(x):
-    cur.execute('select * from url_data')
-    for row in cur:
-        print(row[x])
+    def cursor_iteration(self, x):
+        cur = self.conn.cursor()
+        cur.execute('select * from url_data')
+        for row in cur:
+            print(row[x])
 
-cursor_iteration(1)
+
+
+def main():
+    run = sql_shit()
+    database = "data_dafnt.db"
+    run.create_connection(database)
+    run.getTable()
+
+
+if __name__ == '__main__':
+    main()
+
+
+#
 
 # cur.execute("create table url_data (lettre, page1_url, page_count)")
 
